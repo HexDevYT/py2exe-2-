7299b90058ca7f41f58bb3b77047075c25188177
Coder rudra <83690766+xxdev-specs@users.noreply.github.com>
2021-10-18 16:03:35 +0530

    Create py2exe.py

---
+++ py2exe.py
@@ -0,0 +1,23 @@
+//#1st log on to your terminal of choice(i know that no one asked but i use windows so dont judge me i also use windows cmd
+
+```
+pip install pyinstaller
+```
+//#once it is installed lets go to the second step
+//#for windows:
+```
+pyinstaller --onefile -w C:\Users\Rudra\OneDrive\Documents\codes\example.py
+```
+//#note that this is just an example
+//#for other os
+```
+pyinstaller --onefile -w example.py
+```
+
+dont know if the other os worked but if did follow me and if it didnt... well STILL follow me
+
+outro:
+  sub to: coder("Rudra");
+    follow: CoderRudra in DEVcommunity
+      check my apps on: repl.it
+        and thats it *peace out emoji*
